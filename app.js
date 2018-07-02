ffac395de79d279c5018faf1a50738de3e0c8bff
ilia esartia <iesartia@vaba.co>
2018-07-02 12:30:00 +0400

    Arrow function samples

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 // const os = require('os');
 const _ = require('lodash');
@@ -7,12 +5,33 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+};
 
-const argv = yargs.argv;
+const bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+};
+
+const argv = yargs
+  .command('add', 'Add a new note', {
+      title: titleOptions,
+      body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+      title: titleOptions
+  })
+  .command('remove', 'Remove a note', {
+      title: titleOptions
+  })
+  .help()
+  .argv;
 var command = argv._[0];
-console.log('Command: ', command);
-console.log('Process: ', process.argv);
-console.log('Yargs: ', argv);
 
 if (command === 'add'){
   var note = notes.addNote(argv.title, argv.body);
@@ -24,7 +43,11 @@ if (command === 'add'){
   }
 
 } else if (command === 'list'){
-  notes.getAll();
+
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).`);
+  allNotes.forEach((note) => notes.logNote(note) );
+
 } else if (command === 'read'){
   var note = notes.getNote(argv.title);
   if (note){
